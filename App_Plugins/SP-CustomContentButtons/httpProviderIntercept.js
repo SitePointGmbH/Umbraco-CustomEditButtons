2f3070eeb7e8805fdb19768b77b0c6b9acb2fb9d
Thomas Beckert <tb@sitepoint.de>
2021-01-02 13:01:40 +0100

    Init

---
+++ App_Plugins/SP-CustomContentButtons/httpProviderIntercept.js
@@ -0,0 +1,28 @@
+﻿angular.module('umbraco.services').config([
+   '$httpProvider',
+   function ($httpProvider) {
+
+     
+       $httpProvider.interceptors.push(function ($q) {
+           return {
+               'request': function (request) {
+                   
+                    
+                  
+                    if (request.url.toLowerCase().indexOf('edit.html') !== -1) {
+                        if (location.href.indexOf('content') !== -1) {
+                            console.log("Alte URL: " + request.url);
+                            request.url = '/App_Plugins/SP-CustomContentButtons/customButtons.html';
+                        }
+
+                        console.log(location.href);
+                    }
+                       
+
+
+                   return request || $q.when(request);
+               }
+           };
+       });
+
+   }]);
