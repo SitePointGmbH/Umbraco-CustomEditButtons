af84608b72fe7fcf379d8df3b79a0621683863c5
Thomas Beckert <tb@sitepoint.de>
2021-01-04 16:08:05 +0100

    Routing fixed for version 8

--- App_Plugins/SP-CustomContentButtons/httpProviderIntercept.js
+++ App_Plugins/SP-CustomContentButtons/httpProviderIntercept.js
@@ -7,18 +7,10 @@
            return {
                'request': function (request) {
                    
-                    
-                  
-                    if (request.url.toLowerCase().indexOf('edit.html') !== -1) {
-                        if (location.href.indexOf('content') !== -1) {
-                            console.log("Alte URL: " + request.url);
-                            request.url = '/App_Plugins/SP-CustomContentButtons/customButtons.html';
-                        }
 
-                        console.log(location.href);
-                    }
-                       
-
+                   if (request.url.toLowerCase().indexOf('views/content/edit.html') !== -1) {
+                       request.url = '/App_Plugins/SP-CustomContentButtons/customButtons.html';
+                   } 
 
                    return request || $q.when(request);
                }
